7d2787b8e8897a346e2343a7be7bfb37f2223468
Daniel <69171112+Daniel-McFly@users.noreply.github.com>
2024-01-19 17:30:43 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "assets/hamburger_icon.png";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "assets/x.png";
+    }
+}
+
+function down(){
+    document.body.scrollTop = 2;
+    document.documentElement.scrollTop = 2;
+}
